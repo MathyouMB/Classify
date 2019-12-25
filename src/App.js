a4a6888b80a86adafd489a31c2f8c03feb0d1d9e
Matthew M-B <matthewmacraebovell@outlook.com>
2019-12-25 03:27:49 -0500

    route transitions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { AnimatedSwitch } from 'react-router-transition';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage'
 import cardsPage from './components/cardsPage'
@@ -19,11 +20,18 @@ function App() {
   return (
     <div className="App">
        <Router>
-          <Route path="/login" component={() => <LoginPage profile={profile} setProfile={setProfile}/>} />
-          <Route path="/signup" component={SignupPage} />
-          <Route path="/profile?ID=1" component={profilePage} />
-          <Route path="/cards" component={cardsPage} />
-          <Route path="/match" component={() => <MatchPage  profile={profile}/>} /> 
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className="switch-wrapper"
+        >
+            <Route path="/login" component={() => <LoginPage profile={profile} setProfile={setProfile}/>} />
+            <Route path="/signup" component={SignupPage} />
+            <Route path="/profile?ID=1" component={profilePage} />
+            <Route path="/cards" component={cardsPage} />
+            <Route path="/match" component={() => <MatchPage  profile={profile}/>} /> 
+          </AnimatedSwitch>
        </Router>  
     </div>
   
