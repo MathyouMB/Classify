519e9145d1bcdd141de3aeec6549d20ceb45cf55
Matthew M-B <matthewmacraebovell@outlook.com>
2019-12-26 10:27:48 -0500

    added better swipe buttons

--- src/components/card/Button.js
+++ src/components/card/Button.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 const buttonStyles = {
-  padding: "16px 24px",
+  //padding: "24px 24px",
+  width: "2.7em",
+  height: "2.7em",
   background: "whitesmoke",
   cursor: "pointer",
   border: "none",
   borderRadius: 3,
-  marginTop: "3em"
+  marginTop: "1em",
+  borderRadius: "50%",
+  fontSize: "25px",
+  boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+
 };
 
-const Button = ({ children, onClick }) => (
-  <button onClick={onClick} style={{ ...buttonStyles }}>
+const Button = ({ children, onClick, id }) => (
+  <button id={id} onClick={onClick} style={{ ...buttonStyles }}>
     {children}
   </button>
 );
